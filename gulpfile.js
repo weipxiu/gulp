9a0e5ef003d3f875362b115fb640b2f0592962bd
lijun <30607580@qq.com>
2018-05-20 18:09:23 +0800

    更改代码格式

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
 var gulp = require('gulp');
-var imagemin = require('gulp-imagemin');//引入图片压缩模块
-var scriptmin = require('gulp-uglify');//引入js压缩模块
-var gulpless = require('gulp-less');//引入less转换模块
-var concat = require('gulp-concat');//引入合并代码模块
+var imagemin = require('gulp-imagemin'); //引入图片压缩模块
+var scriptmin = require('gulp-uglify'); //引入js压缩模块
+var gulpless = require('gulp-less'); //引入less转换模块
+var concat = require('gulp-concat'); //引入合并代码模块
+
 /*
 gulp.task -- 定义任务
 gulp.src -- 找到需要执行任务的文件
@@ -12,25 +13,33 @@ gulp.watch -- 观察文件是否发生改变
 执行任务 gulp + 任务名称 + 回车
 */
 
-
 // 拷贝文件
-gulp.task("copyHtml",function(){
+gulp.task("copyHtml", function () {
     //pipe后面对应的地址就是将前面路径文件拷贝复制到哪里去
     gulp.src("src/*.html").pipe(gulp.dest("dist"))
 });
 
 //图片压缩
 //安装模块 npm install --save-dev gulp-imagemin
-gulp.task("imageMin",function(){
-    gulp.src('src/images/*')        
+gulp.task("imageMin", function () {
+    gulp.src('src/images/*')
         .pipe(imagemin([
-            imagemin.gifsicle({interlaced: true}),
-            imagemin.jpegtran({progressive: true}),
-            imagemin.optipng({optimizationLevel: 7}),
+            imagemin.gifsicle({
+                interlaced: true
+            }),
+            imagemin.jpegtran({
+                progressive: true
+            }),
+            imagemin.optipng({
+                optimizationLevel: 7
+            }),
             imagemin.svgo({
-                plugins: [
-                    {removeViewBox: true},
-                    {cleanupIDs: false}
+                plugins: [{
+                        removeViewBox: true
+                    },
+                    {
+                        cleanupIDs: false
+                    }
                 ]
             })
         ]))
@@ -39,16 +48,16 @@ gulp.task("imageMin",function(){
 
 // 压缩js
 //安装js压缩模块 npm i gulp-uglify --save-dev
-gulp.task("scriptmin",function(){
+gulp.task("scriptmin", function () {
     //pipe后面对应的地址就是将前面路径文件拷贝复制到哪里去
-    gulp.src(["src/js/*.js","!src/js/not.js"])
+    gulp.src(["src/js/*.js", "!src/js/not.js"])
         .pipe(scriptmin())
         .pipe(gulp.dest("dist/js"))
 });
 
 // 转行less
 //安装js压缩模块 npm i gulp-less --save-dev
-gulp.task("gulpless",function(){
+gulp.task("gulpless", function () {
     //pipe后面对应的地址就是将前面路径文件拷贝复制到哪里去
     gulp.src("src/css/*.less")
         .pipe(gulpless())
@@ -57,21 +66,20 @@ gulp.task("gulpless",function(){
 
 //代码合并
 //安装 npm i gulp-concat --save-dev
-gulp.task("concat",function(){
+gulp.task("concat", function () {
     gulp.src("src/js/*.js")
-    .pipe(concat("main.js"))
-    .pipe(scriptmin())//在合并的时候压缩js
-    .pipe(gulp.dest("dist/js"))
+        .pipe(concat("main.js"))
+        .pipe(scriptmin()) //在合并的时候压缩js
+        .pipe(gulp.dest("dist/js"))
 })
 
 //监听文件是否发生改变
-gulp.task("watch",function(){
-    gulp.watch("src/*.html",["copyHtml"]);
-    gulp.watch("src/css/*.less",["gulpless"]);
-    gulp.watch("src/images/*",["imageMin"]);
-    gulp.watch("src/js/*.js",["scriptmin"]);
+gulp.task("watch", function () {
+    gulp.watch("src/*.html", ["copyHtml"]);
+    gulp.watch("src/css/*.less", ["gulpless"]);
+    gulp.watch("src/images/*", ["imageMin"]);
+    gulp.watch("src/js/*.js", ["scriptmin"]);
 })
 
 //如果直接执行 gulp 那么就是运行任务名称为‘default’的任务,后面数组代表所需要执行的任务列表
-gulp.task('default', ["copyHtml","gulpless","imageMin","scriptmin"]);
-
+gulp.task('default', ["copyHtml", "gulpless", "imageMin", "scriptmin"]);
